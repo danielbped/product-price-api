376589b6f936ae5dd8e3ff8974ef3beefe551c0a
danielbped <danielbped@gmail.com>
2024-04-16 18:18:39 -0300

    feat: updates errorMessage files

--- src/utils/ErrorMessage.ts
+++ src/utils/ErrorMessage.ts
@@ -1,4 +1,10 @@
 export default {
   ErrorFindingRoutes: 'Encountered Error initializing routes from',
   InternalServerError: 'Internal server error.',
+  ProductNotFount: 'Product not found.',
+  UnexpectedError: 'Unexpected Error.', 
+  MissingProductCode: 'Missing product code.',
+  SalePriceBellowCostPrice: 'Sale price couldn\'t be lower than cost price.',
+  LowerThanItself: 'couldn\'t be 10% lower than itself',
+  HigherThanItself: 'couldn\'t be 10% higher than itself',
 };
