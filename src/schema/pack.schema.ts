3332af9b6fac3a77fc1962922451741d39c12981
danielbped <danielbped@gmail.com>
2024-04-22 16:42:30 -0300

    docs: update getPacks schema

--- src/schema/pack.schema.ts
+++ src/schema/pack.schema.ts
@@ -13,6 +13,21 @@
  *             type: string
  *           product_id:
  *             type: string
+ *           product:
+ *             type: object
+ *             properties:
+ *               code:
+ *                 type: number
+ *               name:
+ *                 type: string
+ *               sales_price:
+ *                 type: number
+ *               cost_price:
+ *                 type: number
+ *               createdAt:
+ *                 type: string
+ *               updatedAt:
+ *                 type: string
  *           value:
  *             type: number
  *           qty:
